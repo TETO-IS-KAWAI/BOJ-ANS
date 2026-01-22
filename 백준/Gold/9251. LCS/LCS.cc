0b794f7450ce4c043cdf9f1c7d915a8c6c0b8199
K4yoko L0ver <k4wait3to@gmail.com>
2026-01-22 21:46:44 +0900

    [Gold IV] Title: LCS, Time: 4 ms, Memory: 6008 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -2,17 +2,13 @@
 using namespace std;
 int table[1010][1010];
 int main(){
-    ios::sync_with_stdio(false);
     string A, B;
     cin>>A>>B;
     for(int i=1;i<=A.length();i++){
         for(int j=1;j<=B.length();j++){
-            if(A[i-1]==B[j-1])
-                table[i][j]=table[i-1][j-1]+1;
-            else
-                table[i][j]=max(table[i-1][j], table[i][j-1]);
+            if(A[i-1]==B[j-1])table[i][j]=table[i-1][j-1]+1;
+            else table[i][j]=max(table[i-1][j], table[i][j-1]);
         }
     }
     cout<<table[A.length()][B.length()];
-    return 0;
 }
