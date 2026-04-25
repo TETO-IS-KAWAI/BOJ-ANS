a7e3378ae64070c55a0c94c9b255ba588f3d006d
K4yoko L0ver <k4wait3to@gmail.com>
2026-04-25 11:47:08 +0900

    [Gold I] Title: 구간 합 구하기, Time: 184 ms, Memory: 26240 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -23,7 +23,7 @@ void update(vector<long long> &a, vector<long long> &tree, int node, int start, 
     tree[node]=tree[node*2]+tree[node*2+1];
 }
 long long query(vector<long long> &tree, int node, int start, int end, int left, int right){
-    if(right<start||end<left){
+    if(left>end||right<start){
         return 0;
     }
     if(left<=start&&end<=right){
@@ -34,22 +34,52 @@ long long query(vector<long long> &tree, int node, int start, int end, int left,
     return lsum+rsum;
 }
 int n, m, k, a, b;
-long long c;
-int main(){
-    scanf("%d%d%d", &n, &m, &k);
-    vector<long long> input(n);
-    vector<long long> tree(4*n);
-    m+=k;
-    for(int i=0;i<n;i++){
-        scanf("%lld", &input[i]);
+long long int c;
+// int main(){
+//     scanf("%d%d", &n, &m, &k);
+//     vector<long long> input(n+1);
+//     for(int i=1;i<=n;i++){
+//         scanf("%lld", &input[i]);
+//     }
+//     vector<long long> tree(4*n+1);
+//     init(input, tree, 1, 1, n);
+//     for(int i=0;i<m+k;i++){
+//         scanf("%d%d%lld", &a, &b, &c);
+//         if(!(a%2)){
+//             printf("%lld\n", query(tree, 1, 1, n, b, c));
+//         }else{
+//             update(input, tree, 1, 1, n, b, c);
+//         }
+//     }
+// }
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<long long> a(n);
+    int h = (int)ceil(log2(n));
+    int tree_size = (1 << (h+1));
+    vector<long long> tree(tree_size);
+    m += k;
+    for (int i=0; i<n; i++) {
+        cin >> a[i];
     }
-    init(input, tree, 1, 0, n-1);
-    for(int i=0;i<m;i++){
-        scanf("%d%d%lld", &a, &b, &c);
-        if(a==1){
-            update(input, tree, 1, 0, n-1, b-1, c);
-        }else{
-            printf("%lld\n", query(tree, 1, 0, n-1, b-1, c-1));
+    init(a, tree, 1, 0, n-1);
+    while (m--) {
+        int what;
+        cin >> what;
+        if (what == 1) {
+            int index;
+            long long val;
+            cin >> index >> val;
+            update(a, tree, 1, 0, n-1, index-1, val);
+        } else if (what == 2) {
+            int left, right;
+            cin >> left >> right;
+            cout << query(tree, 1, 0, n-1, left-1, right-1) << '\n';
         }
     }
+    return 0;
 }
